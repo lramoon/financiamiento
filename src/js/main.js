acadf66a4b8ee01183b2d54786a486eef8d106a9
Ramon <bin.ramon.sh@gmail.com>
2024-06-19 16:35:22 -0400

    Feat: new changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import financiamiento from "../config/info.js";
 import { funciones } from "./componentes/orden.js";
+import { filtros } from "./componentes/filtros.js";
 import actualizarSelectsPorMarca from "./componentes/actualizarPorMarca.js";
 import mostrarDatosSeleccionados from "./componentes/mostrarDatos.js";
 window.addEventListener("DOMContentLoaded", (e) => {
@@ -7,10 +8,21 @@ window.addEventListener("DOMContentLoaded", (e) => {
 	// Event listener para el select de marcas
 	document.getElementById("selectMarca").addEventListener("change", (e) => {
 		actualizarSelectsPorMarca(e.target.value, financiamiento);
+		// Limpiar inputs
+		document.getElementById("inputInicial").value = "";
+		document.getElementById("inputPeriodoDeMeses").value = "";
+		document.getElementById("inputMensualidad").value = "";
+		document.getElementById("inputInteresTotal").value = "";
+		document.getElementById("inputPagosTotales").value = "";
 	});
 	// Agregar event listeners a los selects para ejecutar la función cuando se haga una selección
 	document.getElementById("selectModelo").addEventListener("change", () => {
+		filtros.actualizarAñosPorModelo(financiamiento.vehiculos);
 		mostrarDatosSeleccionados(financiamiento);
+		// Verificar que se haya seleccionado un modelo válido antes de habilitar el selectYear
+		if (document.getElementById("selectModelo").value) {
+			document.getElementById("selectYear").disabled = false;
+		}
 	});
 	document.getElementById("selectYear").addEventListener("change", () => {
 		mostrarDatosSeleccionados(financiamiento);
