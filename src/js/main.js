d0a0b3dc732451067060a3c487aeb1d4b1e7919d
lramoon <bin.ramon.sh@gmail.com>
2024-06-19 21:50:04 -0400

    Feat: remove

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
-window.addEventListener('DOMContentLoaded',(e)=>{
-    
+async function fetchData() {
+    try {
+        const response = await fetch('../../financiamiento.json');
+        const data = await response.json();
+        return data
+        // Aquí puedes manipular los datos obtenidos
+    } catch (error) {
+        console.error('Error al realizar la petición:', error);
+        // Aquí puedes manejar el error, por ejemplo, mostrar un mensaje al usuario
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', async (e) => {
+    let x = await fetchData();
+    console.log(x)
     e.preventDefault()
 })
