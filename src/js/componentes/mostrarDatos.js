caacd73f0117735b3e87551c80999efb60f49e62
Ramon <bin.ramon.sh@gmail.com>
2024-06-21 13:36:38 -0400

    Feat: add sum 16% a los 48 meses

--- src/js/componentes/mostrarDatos.js
+++ src/js/componentes/mostrarDatos.js
@@ -1,18 +1,45 @@
 import { funciones } from "./orden.js";
 
+//Función pura para obtener el pago total basdo en el periiodo seleccionado
+const obtenerPagoTotalPorPeriodo = (vehiculo, periodo) => {
+	const pagoTotal = vehiculo.Precio_con_financiamiento;
+	const indice = periodo === "24" ? 0 : 1;
+	// Si no hay un segundo valor, dividir entre 2 el primer pago
+	let interesSegundoAno = (
+		parseFloat(pagoTotal.replace("$", "")).toFixed(2) *
+		(16 / 100)
+	).toFixed(2);
+
+	if (indice === 1) {
+		return (
+			"$" +
+			Number(parseFloat(pagoTotal.replace("$", ""))) +
+			Number(interesSegundoAno)
+		);
+	} else {
+		return pagoTotal;
+	}
+};
 // Función pura para obtener la mensualidad basada en el periodo seleccionado
 const obtenerMensualidadPorPeriodo = (vehiculo, periodo) => {
 	const mensualidades = vehiculo.PagoMensual;
 	const indice = periodo === "24" ? 0 : 1;
 	// Si no hay un segundo valor, dividir entre 2 el primer pago
+	let pago48meses = (parseFloat(mensualidades[0].replace("$", "")) / 2).toFixed(
+		2
+	);
+	let interesSegundoAno = (
+		(parseFloat(mensualidades[0].replace("$", "")) / 2).toFixed(2) *
+		(16 / 100)
+	).toFixed(2);
 	return (
 		mensualidades[indice] ||
-		"$" + (parseFloat(mensualidades[0].replace("$", "")) / 2).toFixed(2)
+		"$" + (Number(pago48meses) + Number(interesSegundoAno)).toFixed(2)
 	);
 };
 // Función para actualizar la mensualidad en el DOM
-const actualizarMensualidadDOM = (mensualidad) => {
-	document.getElementById("inputMensualidad").value = mensualidad;
+const actualizarMensualidadDOM = (mensualidad, inputID) => {
+	document.getElementById(inputID).value = mensualidad;
 };
 
 // Función de orden superior para manejar el evento change del select de periodos
@@ -22,7 +49,9 @@ const manejarCambioDePeriodo = (vehiculo) => (evento) => {
 		vehiculo,
 		periodoSeleccionado
 	);
-	actualizarMensualidadDOM(mensualidad);
+	const pagoTotal = obtenerPagoTotalPorPeriodo(vehiculo, periodoSeleccionado);
+	actualizarMensualidadDOM(mensualidad, "inputMensualidad");
+	actualizarMensualidadDOM(pagoTotal, "inputPagosTotales");
 };
 
 // Función para inicializar el select de periodos y asignar el event handler
@@ -34,10 +63,7 @@ const inicializarSelectPeriodoDeMeses = (vehiculo) => {
 	);
 };
 
-// Ejemplo de uso:
-// // Suponiendo que 'financiamiento' es tu objeto con la información de los vehículos
-// const vehiculoSeleccionado = financiamiento.vehiculos[0]; // Obtener el vehículo seleccionado
-// inicializarSelectPeriodoDeMeses(vehiculoSeleccionado);
+// Suponiendo que 'financiamiento' es tu objeto con la información de los vehículos
 function mostrarDatosSeleccionados(financiamiento) {
 	// Obtener los valores seleccionados
 	const marcaSeleccionada = document.getElementById("selectMarca").value;
